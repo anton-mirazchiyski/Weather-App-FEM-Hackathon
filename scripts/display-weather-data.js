e2dca4b8e6421e682f4df9a959ef25833eed5374
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-09-18 13:07:37 +0300

    Function to display the current weather data
    Also a function that displays the location name and the country name

--- scripts/display-weather-data.js
+++ scripts/display-weather-data.js
@@ -13,6 +13,8 @@ export async function displayWeatherInfoOfCity() {
     const currentWeatherData = await getCurrentWeatherData(cityLatitude, cityLongitude);
     const [temperature, apparentTemperature, humidity, windSpeed, precipitation] = currentWeatherData;
     // console.log(temperature, apparentTemperature, humidity, windSpeed, precipitation);
+    displayLocationNames(cityName, cityCountry);
+    displayCurrentWeatherData(temperature, apparentTemperature, humidity, windSpeed, precipitation);
 }
 
 
@@ -57,4 +59,24 @@ async function getCurrentWeatherData(latitude, longitude) {
     } catch(error) {
         return null;
     }
+}
+
+
+function displayCurrentWeatherData(temperature, apparentTemperature, humidity, windSpeed, precipitation) {
+    const currentTemperatureElement = document.querySelector('.city-name-container .current-temperature-high');
+    const apparentTemperatureElement = document.querySelector('.apparent-temperature');
+    const humidityElement = document.querySelector('.humidity');
+    const windSpeedElement = document.querySelector('.wind-speed');
+    const precipitationElement = document.querySelector('.precipitation');
+
+    currentTemperatureElement.textContent = `${Math.round(temperature)}°`;
+    apparentTemperatureElement.textContent = `${Math.round(apparentTemperature)}°`;
+    humidityElement.textContent = `${humidity}%`;
+    windSpeedElement.textContent = `${Math.round(windSpeed)} km/h`;
+    precipitationElement.textContent = `${precipitation} mm`;
+}
+
+function displayLocationNames(cityName, countryName) {
+    const nameElement = document.querySelector('.city-info > .city-name');
+    nameElement.textContent = `${cityName}, ${countryName}`;
 }
